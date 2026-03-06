e1d7abfc4eaa16635cc1079ac5db3b47e6cde436
Lapiniot <aossss@gmail.com>
2026-03-06 16:43:55 +0200

    - Switch to module type - Set target to ES2024 and module resolution to NodeNext - npm dependencies bumped to latest versions - package metadata added - minimal supported Node.JS version is bumped to node24 - some breaking changes in action metadata (underscores replaced with dashes for property names, proper optional property defaults specified) - use ncc to compile TypeScript directly - launch.json added with Node.JS debug support

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ async function run() {
 
         const { repo: { owner, repo }, sha } = context;
 
-        const name = getInput('name', { required: true }).replace("refs/tags/", "");
-        const tag = getInput("tag_name", { required: true }).replace("refs/tags/", "");
-        const body = getInput("body", { required: true }).replace("refs/tags/", "");
+        const name = getInput('release-name', { required: true });
+        const tag = getInput("tag-name", { required: true }).replace("refs/tags/", "");
+        const body = getInput("body", { required: true });
         const prerelease = getBooleanInput("prerelease");
         const draft = getBooleanInput("draft");
-        const generateReleaseNotes = getInput("generate_release_notes")?.toUpperCase() === "TRUE";
-        const makeLatest = getInput("make_latest") as "true" | "false" | "legacy";
+        const generateReleaseNotes = getBooleanInput("generate-release-notes");
+        const makeLatest = getInput("make-latest") as "true" | "false" | "legacy";
 
         const response = await client.rest.repos.createRelease({
             owner, repo, name, tag_name: tag, body, target_commitish: sha,
@@ -24,8 +24,8 @@ async function run() {
         const { data: { id, html_url, upload_url } } = response;
 
         setOutput('id', id);
-        setOutput('html_url', html_url);
-        setOutput('upload_url', upload_url);
+        setOutput('html-url', html_url);
+        setOutput('upload-url', upload_url);
     } catch (error) {
         setFailed((error as Error).message);
     }
